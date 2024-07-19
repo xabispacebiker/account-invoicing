546621380c2fe27713c1fe985ddd270733045801
Xabi Spacebiker <1733884+xabispacebiker@users.noreply.github.com>
2024-07-19 21:12:26 +0200

    Update account_move.py
    [FIX] account: Exclude rounding lines from tax validation
    
    Modified the _test_invoice_line_tax method to exclude invoice lines where is_rounding_line is True. This ensures that only relevant invoice lines are checked for missing taxes, preventing unnecessary errors related to rounding lines.

--- account_invoice_tax_required/models/account_move.py
+++ account_invoice_tax_required/models/account_move.py
@@ -16,7 +16,7 @@ class AccountMove(models.Model):
         errors = []
         error_template = _("Invoice has a line with product %s with no taxes")
         for invoice_line in self.mapped("invoice_line_ids").filtered(
-            lambda x: x.display_type is False
+            lambda x: x.display_type is False and not x.is_rounding_line
         ):
             if not invoice_line.tax_ids:
                 error_string = error_template % (invoice_line.name)
